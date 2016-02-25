a1e7809be043117abb830733a6121efc8273f354
Derek <derek.bingham@satalyst.com>
2016-02-25 16:51:36 +0800

    first device id

--- CreateDeviceIndentity/Program.cs
+++ CreateDeviceIndentity/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
 
 namespace CreateDeviceIndentity
 {
     class Program
     {
+        static RegistryManager registryManager;
+        static string connectionString = "connectionstring";
         static void Main(string[] args)
         {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
         }
     }
 }
